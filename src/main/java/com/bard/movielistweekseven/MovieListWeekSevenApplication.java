c1f7299923e87f534f9b88c988d6a95de54e7b40
bard <bartoszslesar@gmail.com>
2024-03-27 19:57:08 +0100

    project initialization

---
+++ src/main/java/com/bard/movielistweekseven/MovieListWeekSevenApplication.java
@@ -0,0 +1,13 @@
+package com.bard.movielistweekseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListWeekSevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieListWeekSevenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bard/movielistweekseven/MovieListWeekSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.movielistweekseven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieListWeekSevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
